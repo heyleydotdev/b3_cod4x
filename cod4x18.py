2aa259697df49c9365735207d67d9fe1b26e1dff
heyley <44564741+heyleydotdev@users.noreply.github.com>
2026-08-21 02:50:49 +0530

    fix: indentation

--- cod4x18.py
+++ cod4x18.py
@@ -19,7 +19,7 @@
 # CHANGELOG:
 #           0.1 - Initial release
 #           0.2 - Added support for B3Hide plugin, force ID64
-#			0.3 - Add player status methods
+#           0.3 - Add player status methods
 
 __author__ = 'Leiizko, heyleydotdev'
 __version__ = '0.3'
@@ -50,27 +50,41 @@ class Cod4X18Parser(b3.parsers.cod4.Cod4Parser):
         """
         Called after the parser is created before run().
         """
-		self.clients.newClient('-1', guid='WORLD', name='World', hide=True, pbid='WORLD')
-		
+        self.clients.newClient(
+            '-1',
+            guid='WORLD',
+            name='World',
+            hide=True,
+            pbid='WORLD'
+        )
+
         blank = self.write('sv_usesteam64id  1', maxRetries=3)
         data = self.write('plugininfo b3hide', maxRetries=3)
+
         if data and len(data) < 50:
-            self._regPlayer = re.compile(r'^\s*(?P<slot>[0-9]+)\s+'
-                                    r'(?P<score>[0-9-]+)\s+'
-                                    r'(?P<ping>[0-9]+)\s+'
-                                    r'(?P<guid>[0-9]+)\s+'
-                                    r'(?P<steam>[0-9]+)\s+'
-                                    r'(?P<name>.*?)\s+'
-                                    r'(?P<ip>(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}'
-                                    r'(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])):?'
-                                    r'(?P<port>-?[0-9]{1,5})\s*', re.IGNORECASE | re.VERBOSE)
-            self._regPlayerShort = re.compile(r'^\s*(?P<slot>[0-9]+)\s+'
-                                         r'(?P<score>[0-9-]+)\s+'
-                                         r'(?P<ping>[0-9]+)\s+'
-                                         r'(?P<guid>[0-9]+)\s+'
-                                         r'(?P<steam>[0-9]+)\s+'
-                                         r'(?P<name>.*?)\s+', re.IGNORECASE | re.VERBOSE)
-								 
+            self._regPlayer = re.compile(
+                r'^\s*(?P<slot>[0-9]+)\s+'
+                r'(?P<score>[0-9-]+)\s+'
+                r'(?P<ping>[0-9]+)\s+'
+                r'(?P<guid>[0-9]+)\s+'
+                r'(?P<steam>[0-9]+)\s+'
+                r'(?P<name>.*?)\s+'
+                r'(?P<ip>(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}'
+                r'(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])):?'
+                r'(?P<port>-?[0-9]{1,5})\s*',
+                re.IGNORECASE | re.VERBOSE
+            )
+
+            self._regPlayerShort = re.compile(
+                r'^\s*(?P<slot>[0-9]+)\s+'
+                r'(?P<score>[0-9-]+)\s+'
+                r'(?P<ping>[0-9]+)\s+'
+                r'(?P<guid>[0-9]+)\s+'
+                r'(?P<steam>[0-9]+)\s+'
+                r'(?P<name>.*?)\s+',
+                re.IGNORECASE | re.VERBOSE
+            )
+
     def unban(self, client, reason='', admin=None, silent=False, *kwargs):
         """
         Unban a client.
@@ -80,6 +94,7 @@ class Cod4X18Parser(b3.parsers.cod4.Cod4Parser):
         :param silent: Whether or not to announce this unban
         """
         result = self.write(self.getCommand('unban', guid=client.guid))
+
         if admin:
             admin.message(result)
 
@@ -90,53 +105,88 @@ class Cod4X18Parser(b3.parsers.cod4.Cod4Parser):
         :param reason: The reason for this tempban
         :param duration: The duration of the tempban
         :param admin: The admin who performed the tempban
-        :param silent: Whether or not to announce this tempban
+        :param silent: Whether or not to announce the tempban
         """
         duration = b3.functions.time2minutes(duration)
+
         if isinstance(client, b3.clients.Client) and not client.guid:
             # client has no guid, kick instead
             return self.kick(client, reason, admin, silent)
+
         elif isinstance(client, str) and re.match('^[0-9]+$', client):
-            self.write(self.getCommand('tempban', cid=client, reason=reason))
+            self.write(
+                self.getCommand('tempban', cid=client, reason=reason)
+            )
             return
+
         elif admin:
             banduration = b3.functions.minutesStr(duration)
-            variables = self.getMessageVariables(client=client, reason=reason, admin=admin, banduration=banduration)
+            variables = self.getMessageVariables(
+                client=client,
+                reason=reason,
+                admin=admin,
+                banduration=banduration
+            )
             fullreason = self.getMessage('temp_banned_by', variables)
+
         else:
             banduration = b3.functions.minutesStr(duration)
-            variables = self.getMessageVariables(client=client, reason=reason, banduration=banduration)
+            variables = self.getMessageVariables(
+                client=client,
+                reason=reason,
+                banduration=banduration
+            )
             fullreason = self.getMessage('temp_banned', variables)
 
         duration = 43200 if int(duration) > 43200 else int(duration)
-        self.write(self.getCommand('tempban', cid=client.cid, reason=reason, duration=duration))
+
+        self.write(
+            self.getCommand(
+                'tempban',
+                cid=client.cid,
+                reason=reason,
+                duration=duration
+            )
+        )
 
         if not silent and fullreason != '':
             self.say(fullreason)
 
-        self.queueEvent(self.getEvent('EVT_CLIENT_BAN_TEMP', {'reason': reason,
-                                                              'duration': duration,
-                                                              'admin': admin}, client))
+        self.queueEvent(
+            self.getEvent(
+                'EVT_CLIENT_BAN_TEMP',
+                {
+                    'reason': reason,
+                    'duration': duration,
+                    'admin': admin
+                },
+                client
+            )
+        )
+
         client.disconnect()
 
-	def getPlayerPings(self, filter_client_ids=None):
+    def getPlayerPings(self, filter_client_ids=None):
         """
         Returns a dict having players' id for keys and players' ping for values.
         :param filter_client_ids: If filter_client_id is an iterable, only return values for the given client ids.
         """
         data = self.write('b3status')
+
         if not data:
             return {}
 
         players = {}
+
         for line in data.split('\n'):
             m = re.match(self._regPlayerShort, line)
+
             if not m:
                 m = re.match(self._regPlayer, line.strip())
-            
+
             if m:
                 players[str(m.group('slot'))] = int(m.group('ping'))
-        
+
         return players
 
     def getPlayerScores(self):
@@ -144,21 +194,25 @@ class Cod4X18Parser(b3.parsers.cod4.Cod4Parser):
         Returns a dict having players' id for keys and players' scores for values.
         """
         data = self.write('b3status')
+
         if not data:
             return {}
 
         players = {}
+
         for line in data.split('\n'):
-            #self.debug('Line: ' + line + "-")
+            # self.debug('Line: ' + line + "-")
             m = re.match(self._regPlayerShort, line)
+
             if not m:
                 m = re.match(self._regPlayer, line.strip())
-            
-            if m:  
+
+            if m:
                 players[str(m.group('slot'))] = int(m.group('score'))
-            #elif '------' not in line and 'map: ' not in line and 'num score ping' not in line:
-                #self.verbose('getPlayerScores() = Line did not match format: %s' % line)
-        
+
+            # elif '------' not in line and 'map: ' not in line and 'num score ping' not in line:
+            #     self.verbose('getPlayerScores() = Line did not match format: %s' % line)
+
         return players
 
     def getPlayerList(self, maxRetries=None):
@@ -168,25 +222,33 @@ class Cod4X18Parser(b3.parsers.cod4.Cod4Parser):
         """
         if self.PunkBuster:
             return self.PunkBuster.getPlayerList()
+
         else:
             data = self.write('b3status', maxRetries=maxRetries)
+
             if not data:
                 return {}
 
             players = {}
             lastslot = -1
+
             for line in data.split('\n')[3:]:
                 m = re.match(self._regPlayer, line.strip())
+
                 if m:
                     d = m.groupdict()
+
                     if int(m.group('slot')) > lastslot:
                         lastslot = int(m.group('slot'))
                         d['pbid'] = None
                         players[str(m.group('slot'))] = d
-                        
+
                     else:
-                        self.debug('Duplicate or incorrect slot number - '
-                                   'client ignored %s last slot %s' % (m.group('slot'), lastslot))
+                        self.debug(
+                            'Duplicate or incorrect slot number - '
+                            'client ignored %s last slot %s' %
+                            (m.group('slot'), lastslot)
+                        )
 
         return players
 
@@ -195,13 +257,14 @@ class Cod4X18Parser(b3.parsers.cod4.Cod4Parser):
         Return the current map/level name.
         """
         data = self.write('b3status')
+
         if not data:
             return None
 
         line = data.split('\n')[0]
         m = re.match(self._reMapNameFromStatus, line.strip())
+
         if m:
             return str(m.group('map'))
 
         return None
-
